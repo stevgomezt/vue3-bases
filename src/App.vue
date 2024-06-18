f4366822eacb9d28d1beec1280f6873b671ff652
stevgomezt <97990647+stevgomezt@users.noreply.github.com>
2024-06-18 14:12:26 -0500

    ajustes

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,7 @@ import { RouterLink, RouterView } from 'vue-router'
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
     <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+      <RouterLink to="/">Vuejs3</RouterLink>
     </div>
   </header>
 
